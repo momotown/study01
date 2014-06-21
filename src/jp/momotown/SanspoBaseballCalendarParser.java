04523045fe05b163afbd9bd2c18bcc15c325a4c8
take441 <take441@take441-Aspire-S3>
2014-06-22 00:28:31 +0900

    日程・結果を解析

--- src/jp/momotown/SanspoBaseballCalendarParser.java
+++ src/jp/momotown/SanspoBaseballCalendarParser.java
@@ -1,10 +1,12 @@
 package jp.momotown;
 
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SanspoBaseballCalendarParser {
@@ -44,11 +46,7 @@ public class SanspoBaseballCalendarParser {
 		m_webDriver.findElement(By.linkText("日程・結果")).click();
 		m_webDriver.findElement(By.linkText((m_calendar.get(Calendar.MONTH) + 1) + "月")).click();
 		
-		for(int i = 0; i < 6; i++) {
-			
-		}
-
-/*
+		/*
  * //table[@id='sj-SC_central03calendar']/tbody/tr[1]		// 列ヘッダだな
  * //table[@id='sj-SC_central03calendar']/tbody/tr[2]/th[1]	// 1週目の日曜日の日
  * //table[@id='sj-SC_central03calendar']/tbody/tr[2]/th[2]	// 1週目の月曜日の日
@@ -56,6 +54,46 @@ public class SanspoBaseballCalendarParser {
  * //table[@id='sj-SC_central03calendar']/tbody/tr[2]/th[4]	// 1週目の水曜日の日
  * //table[@id='sj-SC_central03calendar']/tbody/tr[3]		// 1週目の内容
 */
+		
+		List<WebElement> weeksOfMonth = m_webDriver.findElements(By.xpath("//table[@id='sj-SC_central03calendar']/tbody//tr[@class='date']"));
+		List<WebElement> gamesOfMonth = m_webDriver.findElements(By.xpath("//table[@id='sj-SC_central03calendar']/tbody//tr[@class='gamesSchedule']"));
+
+		/*
+		int i = 0;
+		for(WebElement date : dates) {
+			System.out.println(String.format("LIST[%d] : %s", i, date.getText()));
+			++i;
+		}
+		*/
+		
+		int indexOfWeek = -1;
+		int indexOfDay = -1;
+		String targatDay = Integer.toString(m_calendar.get(Calendar.DAY_OF_MONTH));
+		for(int i = 0; i < weeksOfMonth.size(); ++i) {
+			WebElement week = weeksOfMonth.get(i);
+			List<WebElement> days = week.findElements(By.xpath("th"));
+			for(int j = 0; j < days.size(); ++j) {
+				WebElement day = days.get(j);
+				System.out.println(String.format("weeks[%d][%d] : %s", i, j, day.getText()));
+				if(targatDay.equals(day.getText())) {
+					indexOfWeek = i;
+					indexOfDay = j;
+				}
+			}
+		}
+		
+		WebElement allGamesOfWeek = gamesOfMonth.get(indexOfWeek);
+//		System.out.println(String.format("gamesOfMonth[%d] : %s", indexOfWeek, allGamesOfWeek.getText()));
+		List<WebElement> gamesOfWeek = allGamesOfWeek.findElements(By.xpath("td"));
+//		System.out.println(String.format("gamesOfWeek : %s", gamesOfWeek.getText());
+		WebElement allGamesOfDay = gamesOfWeek.get(indexOfDay);
+//		System.out.println(String.format("gamesOfDay[%d] : %s", indexOfDay, gamesOfDay.getText()));
+		List<WebElement> gamesOfDay = allGamesOfDay.findElements(By.tagName("a"));
+		for(WebElement game : gamesOfDay) {
+			String text = game.getText();
+			String[]  array = text.split(" ");
+			System.out.println(String.format("game : %s", game.getText()));
+		}
 		
 		tearDown();
 		return true;
