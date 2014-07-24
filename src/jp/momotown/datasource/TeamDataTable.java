e79ab39fb7b61e55e83aae54bd0540cfc1c16341
yoshii_takehito <yoshii_takehito@example.com>
2014-07-24 20:20:14 +0900

    更新

--- src/jp/momotown/datasource/TeamDataTable.java
+++ src/jp/momotown/datasource/TeamDataTable.java
@@ -13,7 +13,8 @@ public final class TeamDataTable extends DataTable {
 	private static final TeamDataTable instance = new TeamDataTable();
 
   private TeamDataTable() {
-		List<ColumnDescription> cd = new ArrayList<ColumnDescription>();
+
+  	List<ColumnDescription> cd = new ArrayList<ColumnDescription>();
 		cd.add(new ColumnDescription("id",			ValueType.NUMBER,	"ID"));
 		cd.add(new ColumnDescription("nameS",		ValueType.TEXT,			"名称"));
 		cd.add(new ColumnDescription("nameES",	ValueType.TEXT,			"名称英"));
